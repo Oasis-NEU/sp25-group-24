e7ceba33b39eb733989de83ac25510a564c4fbb8
lilyyyc55 <cordina.l@northeastern.edu>
2025-02-02 13:25:03 -0500

    adding a couple tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,37 @@
-import { Stack } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="circle" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="calander"
+        options={{
+          title: 'Calander',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="anchor" color={color} />,
+        }}
+        />
+        <Tabs.Screen
+        name="events"
+        options={{
+          title: 'Events',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+        />
+    </Tabs>
+  );
 }
