81895c9d258f92f82fbe0ec2291b7f56c270c627
lilyyyc55 <cordina.l@northeastern.edu>
2025-02-09 13:10:49 -0500

    adding a button to events

--- app/events.tsx
+++ app/events.tsx
@@ -1,9 +1,14 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, Alert} from 'react-native';
 
 export default function Tab() {
   return (
     <View style={styles.container}>
       <Text>Tab [Home|Events]</Text>
+      <Button
+          title="X"
+          color="#f194ff"
+          onPress={() => Alert.alert('Button with adjusted color pressed')}
+        />
     </View>
   );
 }
